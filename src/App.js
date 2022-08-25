1a1c96da5f6e341f4770d8b94b980514d01cf75d
Artem Kukharchuk <kuharchuk.as@gmail.com>
2022-08-26 00:43:14 +0300

    load template files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Component from "./components/Component";
+
+const App = () => {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Component.jsx
@@ -0,0 +1,5 @@
+const Component = () => {
+  return <div>Remove or edit this component, it just for testing template</div>;
+};
+
+export default Component;
